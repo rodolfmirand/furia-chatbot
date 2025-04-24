4a7ee2a1cd1330d3623f0f55ed2cf12a65990ca2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-04-24 19:38:12 -0300

    feat: adicionar gemini controller e service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatBotGenerateTextController } from './controllers/chatbot.generate.text.controller';
+import { GeminiService } from './services/gemini.service';
 
 @Module({
   imports: [
@@ -14,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       isGlobal: true
     })
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ChatBotGenerateTextController],
+  providers: [GeminiService],
 })
 export class AppModule {}
