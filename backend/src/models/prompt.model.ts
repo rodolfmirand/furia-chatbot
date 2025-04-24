5218a615c8e65811629bdb9464e05e8581029047
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-04-24 19:36:17 -0300

    feat: criar class model de prompt

---
+++ backend/src/models/prompt.model.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class PromptModel {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column()
+    text: string
+}
