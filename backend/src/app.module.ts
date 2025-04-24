1f69707683e8d93520262dba71d3bce9b4651304
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-04-24 10:41:33 -0300

    chore: configurar database no app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './database.sql',
+      synchronize: true,
+      entities: ['dist/**/*.model.js'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
