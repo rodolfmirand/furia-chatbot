b4df8144282ddcdbe3598ab4b54acc2946567bc9
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-04-24 20:11:21 -0300

    feat: adicionar controllers, providers e exports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatBotGenerateTextController } from './controllers/chatbot.generate.text.controller';
 import { GeminiService } from './services/gemini.service';
+import { PromptFindAllService } from './services/prompt.findall.service';
+import { PromptSaveService } from './services/prompt.save.service';
+import { PromptModel } from './models/prompt.model';
+import { PromptFindAllController } from './controllers/prompt.findall.controller';
 
 @Module({
   imports: [
@@ -14,9 +18,11 @@ import { GeminiService } from './services/gemini.service';
     }),
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    TypeOrmModule.forFeature([PromptModel])
   ],
-  controllers: [ChatBotGenerateTextController],
-  providers: [GeminiService],
+  controllers: [ChatBotGenerateTextController, PromptFindAllController],
+  providers: [GeminiService, PromptFindAllService, PromptSaveService],
+  exports: [PromptFindAllService, PromptSaveService]
 })
 export class AppModule {}
