7cd837e122e218c4d19bcb9ee017dc268e1115ff
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-01 17:21:36 -0300

    feat: funcao para adicionar conversa dinamicamente

--- frontend/src/pages/ChatBot.jsx
+++ frontend/src/pages/ChatBot.jsx
@@ -1,11 +1,49 @@
+import FuriaBot from '../components/furiabot/FuriaBot';
 import Input from '../components/input/Input';
+import FanQuestion from '../components/fanQuestion/FanQuestion';
 import styles from './ChatBot.module.css'
+import { useState } from 'react';
+import { getChatResponse } from '../utils/GetChatResponse';
 function ChatBot() {
+    const [messages, setMessages] = useState([
+        'Bot: Olá, torcedor! Sou um chatbot da Furia. O que gostaria se saber sobre o time?'
+    ]);
+
+    const [input, setInput] = useState('');
+
+    const handleSend = async () => {
+        if (input.trim() === '') return;
+
+        setMessages((prev) => [...prev, `Você:${input}`]);
+
+        try {
+            const response = await getChatResponse(input);
+
+            setMessages((prev) => [...prev, `Bot:${response}`])
+
+            setInput('')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const endRef = useRef(null);
+
     return (
         <div className={styles.chatContainer}>
-            <div className={styles.messages}></div>
+            <div className={styles.messagesArea}>
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        {msg.startsWith('Bot:') ? (
+                            <FuriaBot text={msg.replace('Bot:', '').trim()} />
+                        ) : (
+                            <FanQuestion question={msg.replace('Você:', '').trim()} />
+                        )}
+                    </div>
+                ))}
+            </div>
             <div className={styles.inputArea}>
-                <Input/>
+                <Input question={input} onChange={(e) => setInput(e.target.value)} onSend={handleSend} />
             </div>
         </div>
     );
