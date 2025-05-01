4b235e8860ad8b09bd508e5be2cb34eddcf46511
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-01 17:23:07 -0300

    feat: criar funcao para detectar pressionamento do Enter e adicionar props

--- frontend/src/components/input/Input.jsx
+++ frontend/src/components/input/Input.jsx
@@ -1,15 +1,30 @@
 import Button from '../button/Button';
 import styles from './Input.module.css'
-function Input() {
-    function autoresize(e){
+function Input({ value, onChange, onSend }) {
+    function autoresize(e) {
         e.target.style.height = '20px';
         e.target.style.height = `${e.target.scrollHeight}px`
     }
 
+    function handleKeyDown(e) {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            onSend();
+        }
+    }
+
     return (
         <>
-            <textarea className={styles.chatTextarea} rows={1} placeholder='Digite sua mensagem...' onInput={autoresize}/>
-            <Button />
+            <textarea
+                className={styles.chatTextarea}
+                rows={1}
+                placeholder='Digite sua mensagem...'
+                onInput={autoresize}
+                value={value}
+                onChange={onChange}
+                onKeyDown={handleKeyDown}
+            />
+            <Button onClick={onSend} />
         </>
     );
 }
