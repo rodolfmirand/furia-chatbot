82d3ce0b350144b34e08bb636a8e8ecffae47527
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-01 16:55:01 -0300

    feat: usar props

--- frontend/src/components/furiabot/FuriaBot.jsx
+++ frontend/src/components/furiabot/FuriaBot.jsx
@@ -1,12 +1,12 @@
 import styles from './furiabot.module.css';
 import furiaIcon from '../../assets/furia-esports-logo.png'
-function FuriaBot() {
+function FuriaBot({text}) {
     return (
         <div className={styles.messageContainer}>
             <img className={styles.furiaIcon} src={furiaIcon} alt="" />
             <div className={styles.message}>
                 <div>
-                    <p></p>
+                    <p>{text}</p>
                 </div>
             </div>
         </div>
